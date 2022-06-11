0ff43b5ec40987a8133b99ea85d95cb24b1301cf
bevzzz <dmytro.y.solovei@gmail.com>
2022-06-11 23:53:57 +0200

    Adjust the volume based on the moving average of the typing speed

--- volume.go
+++ volume.go
@@ -1,20 +1,55 @@
 package main
 
+import (
+	"time"
+)
+
+const maxStrokesPerMinute float64 = 200
+const minVolume int = 30
+
+type VolumeController interface {
+	SetVolume(v int)
+	GetVolume() int
+}
+
 type Volume struct {
-	on        bool
-	threshold int
+	strokes []float64
+	interval time.Duration
+	initialVolume int
+	controller VolumeController
 }
 
-func NewVolume(threshold int) *Volume {
-	return &Volume{on: true, threshold: threshold}
-}
-
-func (v *Volume) On() bool {
-	return v.on
-}
-
-func (v *Volume) Adjust(nKeystrokes int) {
-	if nKeystrokes >= v.threshold {
-		v.on = !v.on
+func NewVolume(window, interval time.Duration, vc VolumeController) *Volume {
+	strokes := make([]float64, window / interval)
+	return &Volume{
+		strokes: strokes,
+		interval: interval,
+		initialVolume: vc.GetVolume(),
+		controller: vc,
 	}
+}
+
+func (v *Volume) Adjust(nStrokes int) {
+	nStrokesPerMinute := float64(nStrokes) * float64(time.Minute / v.interval)
+	v.strokes = append(v.strokes[1:], nStrokesPerMinute) // push new value
+	averageStrokes := mean(v.strokes...)
+	reduceVolumeBy := averageStrokes / maxStrokesPerMinute
+	newVolume := int(float64(v.initialVolume) * (1 - reduceVolumeBy))
+	if newVolume < minVolume {
+		newVolume = minVolume
+	}
+
+	v.controller.SetVolume(newVolume)
+}
+
+func (v *Volume) Reset() {
+	v.controller.SetVolume(v.initialVolume)
+}
+
+func mean(numbers ...float64) float64 {
+	var total float64
+	for _, n := range numbers {
+		total += n
+	}
+	return total / float64(len(numbers))
 }
--- volume_test.go
+++ volume_test.go
@@ -1,39 +1,83 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestNewVolume(t *testing.T) {
-	v := NewVolume(1)
-	expectVolumeOn(t, v)
-}
+func TestAdjust(t *testing.T) {
 
-func TestAdjustVolume(t *testing.T) {
+	t.Run("lower boundary is set", func(t *testing.T) {
 
-	t.Run("toggles the volume on and off", func(t *testing.T) {
-		v := NewVolume(1)
+		initialVolume := 87
+		volume, controller := createVolumeAndController(t, initialVolume)
 
-		v.Adjust(1)
-		expectVolumeOff(t, v)
+		volume.Adjust(50)  // would imply -50 volume
 
-		v.Adjust(1)
-		expectVolumeOn(t, v)
+		if controller.Volume < minVolume {
+			t.Errorf("got %d, expected minimum %d", controller.Volume, minVolume)
+		}
 	})
 
-	t.Run("does not adjust below sensitivity threshold", func(t *testing.T) {
-		v := NewVolume(10)
-		v.Adjust(7)
-		expectVolumeOn(t, v)
+	t.Run("sets the volume based on the average char/min", func(t *testing.T) {
+
+		initialVolume := 100
+		volume, controller := createVolumeAndController(t, initialVolume)
+
+		// Typing speed in each interval
+		charactersTyped := []int{3, 5, 4, 6, 1, 1}
+		want := []int{91, 76, 64, 46, 43, 49}
+
+		for i, typed := range charactersTyped {
+
+			volume.Adjust(typed)
+
+			if controller.Volume != want[i] {
+				t.Fatalf(
+					"got %d, want %d, given typed %v, and initial volume %d",
+					controller.Volume, want[i], charactersTyped[:i+1], initialVolume,
+				)
+			}
+		}
 	})
 }
 
-func expectVolumeOn(t *testing.T, v *Volume) {
-	if !v.On() {
-		t.Error("expected the sound to be on")
+func TestReset(t *testing.T) {
+	initialVolume := 70
+	volume, controller := createVolumeAndController(t, initialVolume)
+
+	// Typing speed in each interval
+	for _, typed := range []int{3, 3, 6} {
+		volume.Adjust(typed)
+	}
+
+	volume.Reset()
+
+	if controller.Volume != initialVolume {
+		t.Errorf("got %d, want %d", controller.Volume, initialVolume)
 	}
 }
 
-func expectVolumeOff(t *testing.T, v *Volume) {
-	if v.On() {
-		t.Error("expected the sound to be off")
-	}
+type spyVolumeController struct {
+	Volume int
+}
+
+func (s *spyVolumeController) SetVolume(v int) {
+	s.Volume = v
+}
+
+func (s *spyVolumeController) GetVolume() int {
+	return s.Volume
+}
+
+func createVolumeAndController(t testing.TB, initialVolume int) (v *Volume, sc *spyVolumeController) {
+	t.Helper()
+
+	interval := 2 * time.Second
+	window := 10 * time.Second
+
+	sc = &spyVolumeController{initialVolume}
+	v = NewVolume(window, interval, sc)
+
+	return
 }
