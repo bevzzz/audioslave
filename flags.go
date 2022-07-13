d2f3c042285ec0f9bb454e4f84f15dbd80846a29
AgentNemo <AgentNemo>
2022-07-13 21:49:00 +0200

    fix: flag and testing

--- flags.go
+++ flags.go
@@ -12,7 +12,7 @@ type Config struct {
 }
 
 // parseCommand - parses the command from the cli
-func parseCommand() Config {
+func parseCommand() *Config {
 	config := &Config{
 		MinVolume:  40,
 		AverageCpm: 200,
@@ -20,11 +20,17 @@ func parseCommand() Config {
 		Window:     10 * time.Second,
 	}
 
-	config.MinVolume = *flag.Int("min-volume", config.MinVolume, "set the minimum volume")
-	config.AverageCpm = *flag.Int("average-cpm", config.AverageCpm, "set your average typing speed")
-	config.Interval = *flag.Duration("interval", config.Interval, "change output every N seconds")
-	config.Window = *flag.Duration("window", config.Window, "change output based on last N values")
+	minVolume := flag.Int("min-volume", config.MinVolume, "set the minimum volume")
+	averageCpm := flag.Int("average-cpm", config.AverageCpm, "set your average typing speed")
+	interval := flag.Duration("interval", config.Interval, "change output every N seconds")
+	window := flag.Duration("window", config.Window, "change output based on last N values")
 
 	flag.Parse()
-	return *config
+
+	config.MinVolume = *minVolume
+	config.AverageCpm = *averageCpm
+	config.Interval = *interval
+	config.Window = *window
+
+	return config
 }
