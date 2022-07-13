f7ae26bc41ff0aee729f524082c383963f32ea3a
AgentNemo <AgentNemo>
2022-07-13 21:05:17 +0200

    chore: cleanup command parsing

--- flags.go
+++ flags.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"time"
 )
@@ -12,22 +11,20 @@ type Config struct {
 	Interval, Window time.Duration
 }
 
-func parseFlags(programName string, args []string) (*Config, string, error) {
-	var conf Config
-	var buf bytes.Buffer
+// parseCommand - parses the command from the cli
+func parseCommand() Config {
+	config := &Config{
+		MinVolume:  40,
+		AverageCpm: 200,
+		Interval:   time.Second,
+		Window:     10 * time.Second,
+	}
 
-	flags := flag.NewFlagSet(programName, flag.ContinueOnError)
-	flags.SetOutput(&buf)
+	config.MinVolume = *flag.Int("min-volume", config.MinVolume, "set the minimum volume")
+	config.AverageCpm = *flag.Int("average-cpm", config.AverageCpm, "set your average typing speed")
+	config.Interval = *flag.Duration("interval", config.Interval, "change output every N seconds")
+	config.Window = *flag.Duration("window", config.Window, "change output based on last N values")
 
-	flags.IntVar(&conf.MinVolume, "min-volume", 40, "set the minimum volume")
-	flags.IntVar(&conf.AverageCpm, "average-cpm", 200, "set your average typing speed")
-	flags.DurationVar(&conf.Interval, "interval", time.Second, "change output every N seconds")
-	flags.DurationVar(&conf.Window, "window", 10*time.Second, "change output based on last N values")
-
-	err := flags.Parse(args)
-	return &conf, buf.String(), err
-}
-
-func wantsHelp(err error) bool {
-	return err == flag.ErrHelp
+	flag.Parse()
+	return *config
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/bevzzz/audioslave
 go 1.18
 
 require (
-	github.com/eiannone/keyboard v0.0.0-20220607221834-61a4c843ac01 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220607221834-61a4c843ac01
+	github.com/itchyny/volume-go v0.2.1
+)
+
+require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/itchyny/volume-go v0.2.1 // indirect
 	github.com/moutend/go-wca v0.2.0 // indirect
 	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
 )
